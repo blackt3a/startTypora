055a95bad96c68164c54a670073966e26c45f6f9
blact <1664102260@qq.com>
2023-03-06 21:51:12 +0800

    test that if the target have existed

--- start_typora.sh
+++ start_typora.sh
@@ -1,17 +1,23 @@
 
-
+#如果参数定大于1个
 if [[ $# -gt 1 ]];then
   echo "be careful"
   exit 1
 fi
 
-
+#参数小于等于一个
 if [[ $# -eq 1 ]];then
   echo "2"
   filename=$1
-  touch $filename
+  #文件是否存在
+  if [ ! -f $filename ];then
+    #不存在则创建文件
+    touch $filename
+  fi
+
   nohup typora $filename >/dev/null 2>&1 &
 else
+  #没有给定文件名
   echo "1"
   nohup typora >/dev/null 2>&1 &
 fi
@@ -21,10 +27,5 @@ fi
 
 
 
-# else
-#    touch $1
-#    echo $1
-#  fi
-#}
 
 #start_typora
